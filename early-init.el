e8f46b1c3768ce264a0b4ab0c9236beed04f08f5
aahsnr-configs <ahsanur041@proton.me>
2026-01-18 17:28:21 +0600

    switching to transient fully

--- early-init.el
+++ early-init.el
@@ -89,11 +89,4 @@
               (setq file-name-handler-alist file-name-handler-alist-original))
             101))  ; Run late
 
-(when (and (fboundp 'startup-redirect-eln-cache)
-         (fboundp 'native-comp-available-p)
-         (native-comp-available-p))
-  (startup-redirect-eln-cache
-   (convert-standard-filename
-    (expand-file-name  "var/eln-cache/" user-emacs-directory))))
-
 ;;; early-init.el ends here
