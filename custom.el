0a0a7476053e24252872f47d8e0563816f879c67
aahsnr-configs <ahsanur041@proton.me>
2026-01-21 02:00:11 +0600

    updated

--- custom.el
+++ custom.el
@@ -4,33 +4,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ace-link apheleia avy-zap cape cdlatex citar-embark citar-org-roam consult-dir
-              consult-eglot consult-org-roam consult-todo consult-yasnippet
-              corfu dashboard default-text-scale diredfl dirvish doom-modeline
-              doom-themes dtrt-indent eglot-booster eldoc-mouse embark-consult
-              envrc evil-anzu evil-args evil-collection evil-exchange
-              evil-goggles evil-lion evil-matchit evil-numbers evil-snipe
-              evil-surround evil-tex evil-textobj-tree-sitter evil-vimish-fold
-              evil-visualstar exec-path-from-shell flymake-ruff forge gcmh
-              general git-gutter-fringe git-timemachine hide-mode-line
-              ibuffer-project iedit indent-bars magit-todos marginalia
-              markdown-toc move-text nerd-icons-completion nerd-icons-corfu
-              nerd-icons-ibuffer nix-mode nix-ts-mode orderless org-download
-              org-eldoc org-fragtog org-gtd org-modern org-pomodoro org-remark
-              org-roam-ui pdf-tools popper python-pytest rainbow-delimiters
-              rainbow-mode saveplace-pdf-view solaire-mode sudo-edit tabspaces
-              treemacs-evil treemacs-magit treemacs-nerd-icons treemacs-tab-bar
-              treesit-fold ultra-scroll undo-fu undo-fu-session vertico wgrep
-              yasnippet-capf yasnippet-snippets))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
-   '((eglot-booster :url "https://github.com/jdtsmith/eglot-booster.git")
+   '((org-ibullets :url "https://github.com/jamescherti/org-ibullets.el.git")
+     (eglot-booster :url "https://github.com/jdtsmith/eglot-booster.git")
      (treesit-fold :url "https://github.com/emacs-tree-sitter/treesit-fold.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-hl-change ((t (:foreground "#e0af68" :background unspecified))))
+ '(diff-hl-delete ((t (:foreground "#f7768e" :background unspecified))))
+ '(diff-hl-insert ((t (:foreground "#9ece6a" :background unspecified))))
  '(pdf-annot-list-annotation-face ((t (:foreground "#bb9af7"))))
  '(pdf-annot-list-location-face ((t (:foreground "#565f89"))))
  '(pdf-history-debug-message-face ((t (:foreground "#565f89"))))
