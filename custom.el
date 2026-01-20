7e6ac2ca3300009731074d9600338041e9f7db54
aahsnr-configs <ahsanur041@proton.me>
2026-01-20 17:00:51 +0600

    updated

---
+++ custom.el
@@ -0,0 +1,46 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ace-link apheleia avy-zap cape cdlatex citar-embark citar-org-roam consult-dir
+              consult-eglot consult-org-roam consult-todo consult-yasnippet
+              corfu dashboard default-text-scale diredfl dirvish doom-modeline
+              doom-themes dtrt-indent eglot-booster eldoc-mouse embark-consult
+              envrc evil-anzu evil-args evil-collection evil-exchange
+              evil-goggles evil-lion evil-matchit evil-numbers evil-snipe
+              evil-surround evil-tex evil-textobj-tree-sitter evil-vimish-fold
+              evil-visualstar exec-path-from-shell flymake-ruff forge gcmh
+              general git-gutter-fringe git-timemachine hide-mode-line
+              ibuffer-project iedit indent-bars magit-todos marginalia
+              markdown-toc move-text nerd-icons-completion nerd-icons-corfu
+              nerd-icons-ibuffer nix-mode nix-ts-mode orderless org-download
+              org-eldoc org-fragtog org-gtd org-modern org-pomodoro org-remark
+              org-roam-ui pdf-tools popper python-pytest rainbow-delimiters
+              rainbow-mode saveplace-pdf-view solaire-mode sudo-edit tabspaces
+              treemacs-evil treemacs-magit treemacs-nerd-icons treemacs-tab-bar
+              treesit-fold ultra-scroll undo-fu undo-fu-session vertico wgrep
+              yasnippet-capf yasnippet-snippets))
+ '(package-vc-selected-packages
+   '((eglot-booster :url "https://github.com/jdtsmith/eglot-booster.git")
+     (treesit-fold :url "https://github.com/emacs-tree-sitter/treesit-fold.git"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(pdf-annot-list-annotation-face ((t (:foreground "#bb9af7"))))
+ '(pdf-annot-list-location-face ((t (:foreground "#565f89"))))
+ '(pdf-history-debug-message-face ((t (:foreground "#565f89"))))
+ '(pdf-isearch-batch ((t (:background "#414868" :foreground "#c0caf5"))))
+ '(pdf-isearch-lazy ((t (:background "#414868" :foreground "#c0caf5"))))
+ '(pdf-isearch-match ((t (:background "#e0af68" :foreground "#1a1b26"))))
+ '(pdf-links-read-link ((t (:background "#7aa2f7" :foreground "#1a1b26"))))
+ '(pdf-occur-document-face ((t (:foreground "#7aa2f7"))))
+ '(pdf-occur-page-face ((t (:foreground "#9ece6a"))))
+ '(pdf-sync-backward-matching-page-label ((t (:foreground "#e0af68"))))
+ '(pdf-sync-forward-matching-page-label ((t (:foreground "#e0af68"))))
+ '(pdf-view-rectangle-face ((t (:background "#7aa2f7" :foreground "#1a1b26"))))
+ '(pdf-view-region ((t (:background "#364a82")))))
