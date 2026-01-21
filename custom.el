1346aab9da6a63f0cbb41a09a2ab6c87eb803860
aahsnr-configs <ahsanur041@proton.me>
2026-01-21 20:39:51 +0600

    updated

--- custom.el
+++ custom.el
@@ -4,11 +4,31 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(ace-link apheleia avy-zap cape cdlatex citar-embark citar-org-roam consult-dir
+              consult-eglot consult-org-roam consult-todo consult-yasnippet
+              corfu dashboard default-text-scale diff-hl diredfl dirvish
+              doom-dashboard doom-modeline doom-themes dtrt-indent eglot-booster
+              eldoc-mouse embark-consult envrc evil-anzu evil-args
+              evil-collection evil-exchange evil-goggles evil-lion evil-matchit
+              evil-numbers evil-snipe evil-surround evil-tex
+              evil-textobj-tree-sitter evil-vimish-fold evil-visualstar
+              exec-path-from-shell flymake-ruff forge gcmh general
+              git-timemachine hide-mode-line ibuffer-project iedit indent-bars
+              magit-todos marginalia markdown-toc move-text
+              nerd-icons-completion nerd-icons-corfu nerd-icons-ibuffer nix-mode
+              nix-ts-mode orderless org-download org-fragtog org-gtd
+              org-ibullets org-modern org-pomodoro org-remark org-roam-ui
+              org-super-agenda pdf-tools popper python-pytest rainbow-delimiters
+              rainbow-mode saveplace-pdf-view solaire-mode sudo-edit
+              treemacs-evil treemacs-magit treemacs-nerd-icons treemacs-persp
+              treemacs-projectile treesit-fold ultra-scroll undo-fu
+              undo-fu-session vertico wgrep yasnippet-capf yasnippet-snippets))
  '(package-vc-selected-packages
-   '((org-ibullets :url "https://github.com/jamescherti/org-ibullets.el.git")
+   '((doom-dashboard :url "https://github.com/emacs-dashboard/doom-dashboard.git")
      (eglot-booster :url "https://github.com/jdtsmith/eglot-booster.git")
-     (treesit-fold :url "https://github.com/emacs-tree-sitter/treesit-fold.git"))))
+     (treesit-fold :url "https://github.com/emacs-tree-sitter/treesit-fold.git")
+     (org-ibullets :url "https://github.com/jamescherti/org-ibullets.el.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
