3ab8b5cfc691cb1622568d59a6c3053dc73c0fd2
aahsnr-configs <ahsanur041@proton.me>
2026-08-09 16:10:23 +0600

    chore: tweak LSP options, disable eglot-booster, and update recentf/save-place/history entries

--- var/save-place.el
+++ var/save-place.el
@@ -1,2 +1,2 @@
 ;;; -*- coding: utf-8; mode: lisp-data -*-
-(("/home/ahsan/.config/emacs/config.org" . 32579) ("/home/ahsan/.config/doom/init.el" . 883))
+(("/home/ahsan/Git/common/learning/python/src/sample.py" . 401) ("/home/ahsan/.config/emacs/config.org" . 253878) ("/home/ahsan/.config/doom/init.el" . 883))
