f524d6c0489ad0cc7b8829fc73aabd1be7d6306b
aahsnr-configs <ahsanur041@proton.me>
2026-08-10 23:13:19 +0600

    refactor: restructure Emacs config and update tree-sitter settings

--- var/save-place.el
+++ var/save-place.el
@@ -1,2 +1,4 @@
 ;;; -*- coding: utf-8; mode: lisp-data -*-
-(("/home/ahsan/Git/common/learning/python/src/sample.py" . 401) ("/home/ahsan/.config/emacs/config.org" . 253878) ("/home/ahsan/.config/doom/init.el" . 883))
+(("/home/ahsan/Git/common/learning/python/src/sample3.py" . 1100)
+ ("/home/ahsan/.config/emacs/config.org" . 262124)
+ ("/home/ahsan/.config/emacs/config.bak/config.bak2.org" . 291167))
