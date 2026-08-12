5d730f417b61e99ba460f00224c45e3a294eb863
aahsnr-configs <ahsanur041@proton.me>
2026-08-13 05:49:21 +0600

    chore: disable corfu-history, tweak flycheck hooks, update recentf and save-place entries

--- var/save-place.el
+++ var/save-place.el
@@ -1,2 +1,2 @@
 ;;; -*- coding: utf-8; mode: lisp-data -*-
-(("/home/ahsan/Git/common/learning/python/src/sample.py" . 83) ("/home/ahsan/.config/emacs/config.org" . 174561) ("/home/ahsan/.config/emacs/early-init.el" . 342))
+(("/home/ahsan/Git/common/python/flake.nix" . 283) ("/home/ahsan/Git/common/python/src/sample.py" . 10) ("/home/ahsan/.config/emacs/config.org" . 262382) ("/home/ahsan/.config/emacs/early-init.el" . 342))
