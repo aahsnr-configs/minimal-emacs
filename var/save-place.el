42d08037e8aa61cfe41df32839e8bf6ac54064be
aahsnr-configs <ahsanur041@proton.me>
2026-08-12 06:44:18 +0600

    chore: adjust Emacs init hooks, latency settings, and update saved file paths

--- var/save-place.el
+++ var/save-place.el
@@ -1,2 +1,2 @@
 ;;; -*- coding: utf-8; mode: lisp-data -*-
-(("/home/ahsan/.config/emacs/config.org" . 259470) ("/home/ahsan/Git/common/learning/python/src/sample.py" . 370) ("/home/ahsan/.config/emacs/config.bak/config.bak1.org" . 343316) ("/home/ahsan/.config/emacs/config.bak/config.bak3.org" . 30688) ("/home/ahsan/.config/doom/config.org" . 36722))
+(("/home/ahsan/.config/emacs/config.org" . 120472) ("/home/ahsan/Git/common/learning/python/src/sample.py" . 518) ("/home/ahsan/.config/emacs/early-init.el" . 342))
